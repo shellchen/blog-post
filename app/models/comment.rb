6c16449cc9852ab58033d238f806dbbabcb9d366
Shellchen <bellhird@gmail.com>
2013-09-30 13:37:26 +0800

    extract vote to module

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,15 +1,16 @@
 class Comment < ActiveRecord::Base
+  include Voteable
   validates_presence_of :content, message: "Content can\'t be blank."
 
-  has_many :votes, as: :voteable
+  # has_many :votes, as: :voteable
   belongs_to :user
   belongs_to :post
 
-  def plus_vote_count
-    self.votes.where(vote: true).count
-  end
+  # def plus_vote_count
+  #   self.votes.where(vote: true).count
+  # end
 
-  def minus_vote_count
-    self.votes.where(vote: false).count
-  end
+  # def minus_vote_count
+  #   self.votes.where(vote: false).count
+  # end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,12 @@
 class Post < ActiveRecord::Base
+  include Voteable
   validates_presence_of :title, message: "Title can\'t be blank."
   
   belongs_to :user
   has_many :comments
   has_many :category_postships
   has_many :categories, through: :category_postships
-  has_many :votes, as: :voteable
+  # has_many :votes, as: :voteable
 
   after_validation :generate_slug
 
@@ -13,13 +14,13 @@ class Post < ActiveRecord::Base
     self.user
   end
 
-  def plus_vote_count
-    self.votes.where(vote: true).count
-  end
+  # def plus_vote_count
+  #   self.votes.where(vote: true).count
+  # end
 
-  def minus_vote_count
-    self.votes.where(vote: false).count
-  end
+  # def minus_vote_count
+  #   self.votes.where(vote: false).count
+  # end
 
   def generate_slug
     self.slug = self.title.gsub(" ", "-").downcase
